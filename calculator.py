bf6c4b1fb7ac15dd9b782cf4e914622e272cedd4
Redstomite <rahul.r.prabhu@gmail.com>
2018-07-22 16:54:32 +0530

    Addition and Subtraction Implimentation

---
+++ calculator.py
@@ -0,0 +1,9 @@
+print("Welcome to pycalc v0.1")
+c = input("What do you want to do?")
+a = int(input("Enter The first number"))
+b = int(input("Enter the second number"))
+if c == "+":
+	print("The answer is:",a+b)
+elif c == "-":
+	print("The answer is:",a-b)
+print("We will add division and multiplication in version 0.2")
