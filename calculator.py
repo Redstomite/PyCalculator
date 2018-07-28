af4f5aa121eb10e4173a270ebe17fb3d80e81844
Redstomite <rahul.r.prabhu@gmail.com>
2018-07-28 13:56:09 +0530

    Asks for your name

--- calculator.py
+++ calculator.py
@@ -1,12 +1,37 @@
-print("Welcome to pycalc v0.1")
-print("My name is pybot")
-name=str(input("What is your name?"))
-print("Hello,",name)
-c = input("What do you want to do?",name,"	")
-a = int(input("Enter The first number	"))
-b = int(input("Enter the second number	"))
+#Rahul Pycalc v0.2
+class Calculator():
+
+	name = None
+
+	def __init__(self, name):
+		self.name = name
+
+	def add(self, a , b):
+		addresult = a+b	
+		return(addresult)
+
+	def sub(self, a , b):
+		return(a-b)
+
+	def times(self, a , b):
+		return(a*b)
+
+	def div(self, a , b):
+		return(a/b)
+obj1 = Calculator("Pycalc")
+print("Welcome to pycalc v0.2")
+a = float(input("What is the first number?  "))
+b = float(input("What is the second number?  "))
+c = input("What do you want to do?(+ , - , / , *)  ")
 if c == "+":
-	print("The answer is:",a+b)
+	results = obj1.add(a,b)
 elif c == "-":
-	print("The answer is:",a-b)
-print("We will add division and multiplication in version 0.2")
+	results = obj1.sub(a,b)
+elif c == "/":
+	results = obj1.div(a,b)
+elif c == "*":
+	results = obj1.times(a,b)
+else:
+	results = "Invalid"
+print(results)
+
